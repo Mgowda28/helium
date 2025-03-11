c71502ad8b92f6062529e18123ae18026f463a9b
Mgowda28 <muddaveregowda.1si21is028@gmail.com>
2025-03-11 15:49:57 +0530

    updat index

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 
+
+const reminders:string[]=[];
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/health',(c)=>{
+  return c.json({message:"hello world"},200);
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get('/reminders',(context)=>{
+  return context.json(reminders,200);
+});
+
+app.post('/reminders',async (context)=>{
+  const body = await context.req.json();
+
+  const reminder = body["reminder"];
+
+  reminders.push(reminder);
+  return context.json(reminders,201);
+});
+
+serve(app);
+
+console.log("Server is running on http://localhost:3000")
